5b1172e7c971760f5208fddee017c309a3e95163
David B <d.jbradley.ii@gmail.com>
2020-01-19 15:49:38 -0800

    Adds UserTrades, UserTeamRequests and UserInventory components to Dashboard component

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import UserTeamRequests from '../UserTeamRequests/UserTeamRequests';
 import UserTrades from '../UserTrades/UserTrades';
-import Inventory from '../Inventory/Inventory';
+import UserInventory from '../UserInventory/UserInventory';
 import './Dashboard.css';
 
 export default class Dashboard extends React.Component{
     render(){
         return(
             <section className="dashboard">
+                <p className="user-info">Teams show up based on your user stats.</p>
                 <UserTrades />
                 <UserTeamRequests />
-                <Inventory />
+                <UserInventory />
             </section>
         );
     }
