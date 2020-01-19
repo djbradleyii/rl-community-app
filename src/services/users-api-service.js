12f35f0d85d96d0c34c169a2791e302cd059335e
David B <d.jbradley.ii@gmail.com>
2020-01-19 15:25:12 -0800

    Adds users-api-service and updates to match server requirements

---
+++ src/services/users-api-service.js
@@ -0,0 +1,96 @@
+import TokenService from './token-service';
+import config from '../config';
+
+const UsersApiService = {
+  getAllUsers() {
+    return fetch(`${config.API_ENDPOINT}/users`, {
+      headers: {
+        authorization: `Bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          return response.json().then((responseJson) => Promise.reject(responseJson));
+        }
+        return response.json();
+      });
+  },
+  getUserById(userid) {
+    return fetch(`${config.API_ENDPOINT}/users/${userid}`, {
+      headers: {
+        authorization: `Bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          return response.json().then((responseJson) => Promise.reject(responseJson));
+        }
+        return response.json();
+      });
+  },
+  getAllItemsByUserId(userid) {
+    return fetch(`${config.API_ENDPOINT}/users/${userid}`, {
+      headers: {
+        authorization: `Bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          return response.json().then((responseJson) => Promise.reject(responseJson));
+        }
+        return response.json();
+      });
+  },
+  insertUser(newUser) {
+    return fetch(`${config.API_ENDPOINT}/users`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        authorization: `Bearer ${TokenService.getAuthToken()}`,
+      },
+      body: JSON.stringify({
+        newUser,
+      }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          return response.json().then((responseJson) => Promise.reject(responseJson));
+        }
+        return response.json();
+      });
+  },
+  deleteUser() {
+    return fetch(`${config.API_ENDPOINT}/users`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        authorization: `Bearer ${TokenService.getAuthToken()}`,
+      },
+      body: JSON.stringify({
+      }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          return response.json().then((responseJson) => Promise.reject(responseJson));
+        }
+        return response.json();
+      });
+  },
+  updateUserById(userUpdates) {
+    return fetch(`${config.API_ENDPOINT}/users`, {
+      method: 'PATCH',
+      headers: {
+        'content-type': 'application/json',
+        authorization: `Bearer ${TokenService.getAuthToken()}`,
+      },
+      body: JSON.stringify(userUpdates),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          return response.json().then((responseJson) => Promise.reject(responseJson));
+        }
+      });
+  },
+};
+
+export default UsersApiService;
