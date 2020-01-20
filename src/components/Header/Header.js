5371c93b25a33ed46b52811a982b49268ac3730a
David B <d.jbradley.ii@gmail.com>
2020-01-20 05:37:21 -0800

    Adds logic to Header component with logic utilizing API data

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Nav from '../Nav/Nav';
+import TokenService from '../../services/token-service';
+import ContextManager from '../../context/context-manager';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
-export default function Header(){
-    return(
-        <header>
-            <h1>Rocket League Community App</h1>
-            <Nav />
-        </header>
-    );
+export default class Header extends React.Component {
+    static contextType = ContextManager;
+    render(){
+        const gamertag = this.context.activeUserData.stats ? this.context.activeUserData.stats.gamertag : " ";
+        return(
+            <header>
+                {TokenService.hasAuthToken() ? <Link to={`/dashboard/${gamertag}`} className="main-header"><h1>Rocket League Community App</h1></Link> : <Link to="/" className="main-header"><h1>Rocket League Community App</h1></Link>}
+                <Nav history={this.props.history}/>
+            </header>
+        )
+    }
 }
