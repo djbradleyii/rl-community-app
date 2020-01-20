707f2707bba3661485ac7f86145f10a4edd5d941
David B <d.jbradley.ii@gmail.com>
2020-01-19 16:08:38 -0800

    Adds Nav links - needs updating to account for logged in users

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,6 +6,12 @@ export default class Nav extends React.Component{
     render(){
         return(
             <ul className="top-nav-container">
+                <Link to={`/signin`} className="nav-link"><li>Sign in</li></Link>
+                <Link to={`/register`} className="nav-link"><li>Register</li></Link>
+                <Link to={`/dashboard/:userId`} className="nav-link"><li>Dashboard</li></Link>
+                <Link to={`/add-inventory-item`} className="nav-link"><li>Add Inventory</li></Link>
+                <Link to={`/teams`} className="nav-link"><li>Team Board</li></Link>
+                <Link to={`/signout`} className="nav-link"><li>Logout</li></Link>
             </ul>
         );
     }
