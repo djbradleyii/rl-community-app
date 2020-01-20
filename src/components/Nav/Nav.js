212e212503b582817c0248e11937ed1cc3969bce
David B <d.jbradley.ii@gmail.com>
2020-01-20 05:39:53 -0800

    Adds structure from static version to  Nav component with logic utilizing API data

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.css';
+import TokenService from '../../services/token-service';
+import ActiveUserService from '../../services/activeuser-service';
 
-export default class Nav extends React.Component{
-    render(){
-        return(
-            <ul className="top-nav-container">
-                <Link to={`/signin`} className="nav-link"><li>Sign in</li></Link>
-                <Link to={`/register`} className="nav-link"><li>Register</li></Link>
-                <Link to={`/dashboard/:userId`} className="nav-link"><li>Dashboard</li></Link>
-                <Link to={`/add-inventory-item`} className="nav-link"><li>Add Inventory</li></Link>
-                <Link to={`/teams`} className="nav-link"><li>Team Board</li></Link>
-                <Link to={`/signout`} className="nav-link"><li>Logout</li></Link>
-            </ul>
-        );
+function Nav(props) {
+    function handleLogoutClick() {
+        TokenService.clearAuthToken();
+        ActiveUserService.clearUserData();
+        props.history.push('/logout');
     }
+
+  function displayNav() {
+    const authorizationStatus = TokenService.hasAuthToken();
+    if (authorizationStatus) {
+      return (
+        <nav role="navigation" className="top-nav-container">
+            <Link to={`/dashboard/:gamertag`} className="nav-link"><li>Dashboard</li></Link>
+            <Link to={`/add-inventory-item`} className="nav-link"><li>Add Inventory</li></Link>
+            <Link to={`/teams`} className="nav-link"><li>Team Board</li></Link>
+            <Link to={`/logout`} className="nav-link" onClick={handleLogoutClick}><li>Logout</li></Link>
+        </nav>
+      );
+    }
+    return (
+      <nav role="navigation" className="not-logged-in">
+        <Link to="/signin" className="nav-link">Sign In</Link>
+        <Link to="/register" className="nav-link">Register</Link>
+      </nav>
+    );
+  }
+  return (
+    displayNav()
+  );
 }
+
+export default Nav;
