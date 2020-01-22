dfcb637e228ecfc7949d83f7fa22ccc056aa436a
David B <d.jbradley.ii@gmail.com>
2020-01-22 04:37:58 -0800

    Adds CSS Styling and necessary classnames for styling

--- src/components/EditProfileForm/EditProfileForm.js
+++ src/components/EditProfileForm/EditProfileForm.js
@@ -55,69 +55,70 @@ export default class EditProfileForm extends React.Component{
         return(
             <form id="edit-profile-form" onSubmit={this.handleSubmit} >
                 <div className="error-message">{!!this.context.errorMessage && this.context.errorMessage}</div>
-                <div className="info">Required Fields</div>
-                <div>
-                    <label htmlFor="platform">Platform:</label>
-                    <select id="platform" name="platform" defaultValue={userData.platform}>
-                        <option value="PC">PC</option>
-                        <option value="PS4">PS4</option>
-                        <option value="Nintendo Switch">Nintendo Switch</option>
-                        <option value="Xbox One">Xbox One</option>
-                    </select>
-                </div>
-                <div>
-                    <label htmlFor="gamertag">Gamertag:</label>
-                    <input type="text" id="gamertag" placeholder="ex. Savage Goalie 3024" name="gamertag"  defaultValue={userData.gamertag}/>
-                </div>
-                <div>
-                    <label htmlFor="rocketid">RocketID:</label>
-                    <input type="text" id="rocketid" placeholder="ex. SavageGoalie3024#123" name="rocketid"  defaultValue={userData.rocket_id}/>
-                </div>
-                <div>
-                    <label htmlFor="register-rank">Rank:</label>
-                    <select id="register-rank" name="rank"  defaultValue={userData.rank}>
-                      <option value="Grand Champion">Grand Champion</option>
-                      <option value="Champion III">Champion III</option>
-                      <option value="Champion II">Champion II</option>
-                      <option value="Champion I">Champion I</option>
-                      <option value="Diamond III">Diamond III</option>
-                      <option value="Diamond II">Diamond II</option>
-                      <option value="Diamond I">Diamond I</option>
-                      <option value="Platinum III">Platinum III</option>
-                      <option value="Platinum II">Platinum II</option>
-                      <option value="Platinum I">Platinum I</option>
-                      <option value="Gold III">Gold III</option>
-                      <option value="Gold II">Gold II</option>
-                      <option value="Gold I">Gold I</option>
-                      <option value="Silver III">Silver III</option>
-                      <option value="Silver II">Silver II</option>
-                      <option value="Silver I">Silver I</option>
-                      <option value="Bronze III">Bronze III</option>
-                      <option value="Bronze II">Bronze II</option>
-                      <option value="Bronze I">Bronze I</option>
-                      <option value="Unranked">Unranked</option>
-                    </select>
-                </div>
-                <div>
-                    <label htmlFor="register-rank-division">Division:</label>
-                    <select id="register-rank-division" name="division" defaultValue={userData.division}>
-                        <option value={null}></option>
-                        <option value="IV">IV</option>
-                        <option value="III">III</option>
-                        <option value="II">II</option>
-                        <option value="I">I</option>
-                    </select>
-                </div>
-                <div>
-                    <label htmlFor="register-lft">Looking for team?</label>
-                    <select id="register-lft" name="lft" defaultValue={userData.lft}>
-                        <option value={true}>Yes</option>
-                        <option value={false}>No</option>
-                    </select>
-                </div>
-                <div>
-                    <label htmlFor="user-summary">Bio:</label>
-                    <textarea id="user-summary" name="bio"  defaultValue={userData.bio}></textarea>
+                <div className="edit-profile-info">
+                    <div>
+                        <label htmlFor="platform">Platform:</label>
+                        <select id="platform" name="platform" defaultValue={userData.platform}>
+                            <option value="PC">PC</option>
+                            <option value="PS4">PS4</option>
+                            <option value="Nintendo Switch">Nintendo Switch</option>
+                            <option value="Xbox One">Xbox One</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="register-rank">Rank:</label>
+                        <select id="register-rank" name="rank"  defaultValue={userData.rank}>
+                        <option value="Grand Champion">Grand Champion</option>
+                        <option value="Champion III">Champion III</option>
+                        <option value="Champion II">Champion II</option>
+                        <option value="Champion I">Champion I</option>
+                        <option value="Diamond III">Diamond III</option>
+                        <option value="Diamond II">Diamond II</option>
+                        <option value="Diamond I">Diamond I</option>
+                        <option value="Platinum III">Platinum III</option>
+                        <option value="Platinum II">Platinum II</option>
+                        <option value="Platinum I">Platinum I</option>
+                        <option value="Gold III">Gold III</option>
+                        <option value="Gold II">Gold II</option>
+                        <option value="Gold I">Gold I</option>
+                        <option value="Silver III">Silver III</option>
+                        <option value="Silver II">Silver II</option>
+                        <option value="Silver I">Silver I</option>
+                        <option value="Bronze III">Bronze III</option>
+                        <option value="Bronze II">Bronze II</option>
+                        <option value="Bronze I">Bronze I</option>
+                        <option value="Unranked">Unranked</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="register-rank-division">Division:</label>
+                        <select id="register-rank-division" name="division" defaultValue={userData.division}>
+                            <option value={null}></option>
+                            <option value="IV">IV</option>
+                            <option value="III">III</option>
+                            <option value="II">II</option>
+                            <option value="I">I</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="register-lft">Looking for team?</label>
+                        <select id="register-lft" name="lft" defaultValue={userData.lft}>
+                            <option value={true}>Yes</option>
+                            <option value={false}>No</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="gamertag">Gamertag:</label>
+                        <input type="text" id="gamertag" placeholder="ex. Savage Goalie 3024" name="gamertag"  defaultValue={userData.gamertag}/>
+                    </div>
+                    <div>
+                        <label htmlFor="rocketid">RocketID:</label>
+                        <input type="text" id="rocketid" placeholder="ex. SavageGoalie3024#123" name="rocketid"  defaultValue={userData.rocket_id}/>
+                    </div>
+                    <div>
+                        <label htmlFor="user-summary">Bio:</label>
+                        <textarea id="user-summary" name="bio"  defaultValue={userData.bio}></textarea>
+                    </div>
                 </div>
                 <div>
                     <button type="submit">Submit</button>
