f0170dbfa3ec12d8815aabfee3a3e264a3c5c80e
David B <d.jbradley.ii@gmail.com>
2020-01-23 02:45:46 -0800

    Adds Demo account to the landing page as well as updates the 'instructions'

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -5,31 +5,36 @@ import './LandingPage.css';
 function LangingPage() {
   return (
     <section className="landing-page">
-      <p>The Rocket League Trading App is where you can trade unwanted items, find desired items and find a teammates.  After you have created an account, you can begin your journey to expanding your Rocket League network.</p>
+      <p>The Rocket League Community App is where you post your inventory, view user's inventory, and find users to join your squad.  Create your account and begin your journey expanding your Rocket League network.</p>
 
       <p>As a User, you have access to the following features:</p>
 
       <p>
-        <b>Create &amp; view Trades</b>
+        <b>Add your personal inventory</b>
         {' '}
-        - Create trades based on your personal inventory.
-        - Search/View trades other users posted.
-        - Coming soon: Post trades based on items you desire.
+        - Add Inventory to your account.
+        - Remove items from inventory that have been traded.
+        - Search your inventory by Category, Color, Rarity or Attribute.
       </p>
 
       <p>
-        <b>Create &amp; view Team Requests</b>
+        <b>Search for teammates</b>
         {' '}
-        - Create Team Requests.
-        - Search/View Team Requests based on search criteria.
-        - Your Dashboard will only show Team Requests based on your rank and platform. 
+        - Users who share your rank will be displayed on your dashboard.
+        - Search/View users based on search criteria of Platform, Rank or Division.
       </p>
 
       <p>
-        <b>Manage your Inventory, Trades and Team Requests</b>
+        <b>Edit Personal Data</b>
         {' '}
-        - After creating, viewing, and accepting trade/team requests, you can return to your Dashboard to see all of the details for each. Here you can confirm trades so that they can be removed from your inventory or cancel the trade if it never took place. Team requests will just show as cards on your dashboard based on your rank and platform.
+        - Update Platform, Rank, or Division to ensure that the Available teams' section of your Dashboard is current. Always keep your information current so that other users can find you.
       </p>
+      <p>
+        <b>Users Posted Inventory</b>
+        {' '}
+        - Click on the Gamertag to view the users details and their posted inventory.
+      </p>
+      <p>Demo Account: Demoaccount@gmail.com Password: DemoPassword1!</p>
       <p>Enjoy!</p>
     </section>
   );
