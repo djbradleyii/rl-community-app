4660e247b8b93b717cd2ed1c875521dd2fc33b5a
David B <d.jbradley.ii@gmail.com>
2020-01-23 01:45:30 -0800

    Adds test for UserInfo - Passing

---
+++ src/components/UserInfo/UserInfo.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import UserInfo from './UserInfo';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <BrowserRouter>
+      <UserInfo />
+    </BrowserRouter>
+  , div);
+  ReactDOM.unmountComponentAtNode(div);
+});
